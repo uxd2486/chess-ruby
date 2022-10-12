559e1547acaee875c8b6067e0157e8b84df6f6d3
Utkarsh Dayal <uxd2486@rit.edu>
2022-10-12 17:10:17 +0530

    Added a PieceManager to keep track of pieces

--- lib/board.rb
+++ lib/board.rb
@@ -1,8 +1,11 @@
 # frozen_string_literal: true
 
+require_relative 'piece_manager'
+
 # Represents the chess board.
 class Board
   def initialize
-    puts
+    @white_pieces = PieceManager.new(true)
+    @black_pieces = PieceManager.new(false)
   end
 end
---
+++ lib/piece_manager.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+# Keeps track of all the pieces of a
+# single colour.
+class PieceManager
+  def initialize(is_white)
+    @pieces = []
+  end
+end
