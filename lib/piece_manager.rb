41ce9776206bfe0027471204a7726c6979435bc7
Utkarsh Dayal <uxd2486@rit.edu>
2022-10-12 17:28:09 +0530

    Stubbed out functions to initialize pieces

--- lib/piece_manager.rb
+++ lib/piece_manager.rb
@@ -4,6 +4,35 @@
 # single colour.
 class PieceManager
   def initialize(is_white)
-    @pieces = []
+    @pieces = init_pawns(is_white)
+    @pieces += init_rooks(is_white)
+    @pieces += init_knights(is_white)
+    @pieces += init_bishops(is_white)
+    @pieces += init_queen(is_white)
+    @pieces += init_king(is_white)
+  end
+
+  def init_pawns(is_white)
+    []
+  end
+
+  def init_rooks(is_white)
+    []
+  end
+
+  def init_knights(is_white)
+    []
+  end
+
+  def init_bishops(is_white)
+    []
+  end
+
+  def init_queen(is_white)
+    []
+  end
+
+  def init_king(is_white)
+    []
   end
 end
