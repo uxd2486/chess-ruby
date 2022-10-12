929f5a3c58f4ffccd6f1306b50410c7e0e1ecc34
Utkarsh Dayal <uxd2486@rit.edu>
2022-10-12 12:33:21 +0530

    Simplified can_move? for king and removed unnecessary constructor

--- lib/king.rb
+++ lib/king.rb
@@ -4,21 +4,8 @@ require_relative 'piece'
 
 # Represents the King on the chess board.
 class King < Piece
-  def initialize(location)
-    super(location)
-    @move_transformations = [[1, 1],
-                             [1, -1],
-                             [-1, 1],
-                             [-1, -1],
-                             [1, 0],
-                             [-1, 0],
-                             [0, 1],
-                             [0, -1]].freeze
-  end
-
   def can_move?(new_location)
-    @move_transformations.any? do |row, col|
-      @location == [row + new_location[0], col + new_location[1]]
-    end
+    (@location[0] - new_location[0]).between?(-1, 1) &&
+      (@location[1] - new_location[1]).between?(-1, 1)
   end
 end
