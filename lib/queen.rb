9d9676f231851765c58a08640269212bdd185163
Utkarsh Dayal <uxd2486@rit.edu>
2022-10-12 16:26:04 +0530

    Added the queen

---
+++ lib/queen.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require_relative 'piece'
+
+# Represents the Queen on the chess board.
+class Queen < Piece
+  def can_move?(new_location)
+    can_move_like_bishop?(new_location) || can_move_like_rook?(new_location)
+  end
+
+  def can_move_like_bishop?(new_location)
+    (@location[0] - new_location[0]).abs == (@location[1] - new_location[1]).abs
+  end
+
+  def can_move_like_rook?(new_location)
+    can_move_horizontally?(new_location) || can_move_vertically?(new_location)
+  end
+
+  def can_move_horizontally?(new_location)
+    new_location[0] == @location[0]
+  end
+
+  def can_move_vertically?(new_location)
+    new_location[1] == @location[1]
+  end
+end
